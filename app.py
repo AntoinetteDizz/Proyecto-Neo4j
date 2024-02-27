c36efdfb2ff5d8a7c7289915f332d1562d2c42a5
Kevinherrer1 <kevinherrerak14@gmail.com>
2024-02-27 02:09:12 -0400

    crear la primera ruta

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-#Codigo
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello word' 
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
